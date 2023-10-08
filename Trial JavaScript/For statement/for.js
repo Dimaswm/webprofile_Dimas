bc631d5be231e17deb97585e07a68a6dfbf25e78
Dimas Wahyu Mubarak <122709698+Dimaswm@users.noreply.github.com>
2023-10-08 16:16:56 +0700

    Add files via upload

---
+++ Trial JavaScript/For statement/for.js
@@ -0,0 +1,14 @@
+const characters = ['Kadita', 'Diggie', 'Arlott', 'Silvanna'];
+
+let para = document.querySelector('p');
+let info = `The characters are `;
+
+for (let i = 0; i < characters.length; i++) {
+    if (i === characters.length - 1) {
+        info += `and ${characters[i]}. `;
+    } else {
+        info += `${characters[i]}, `;
+    }
+}
+
+para.innerText = info;
---
+++ Trial JavaScript/Funtion/function.js
@@ -0,0 +1,9 @@
+let greeting = "Halo allerseits!";
+
+function games() {
+    let title = "The Sims 4"; 
+    console.log(title, greeting);
+}
+
+games();
+console.log(greeting);
